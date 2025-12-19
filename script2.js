4fafb047e3197951b340ae82b3d5677ec8560392
love2sketch <243413737+Love2Sketch@users.noreply.github.com>
2025-12-19 17:28:29 -0500

    feat: make rounds execute in game function
    New answers for each round don't work yet

--- script2.js
+++ script2.js
@@ -1,7 +1,7 @@
 // generate random number and calculate computer answer
 let compAns = Math.random();
 
-function getComputerChoice(compAns){
+function getComputerChoice(){
     if (compAns <= 0.3333333) {
         return "rock";
     }
@@ -12,18 +12,17 @@ function getComputerChoice(compAns){
         return "scissors";
     }
 }
-// run computer answer converter function
-getComputerChoice(compAns);
 
 // display computer answer
 console.log(getComputerChoice(compAns));
+
 // console.log(typeof(getComputerChoice(compAns))) // confirmed this is returning a string
 
 // ask for human answer
 let humAns = prompt("rock, paper, or scissors?").toLowerCase();
 
-// use function as TOP suggests... seems unnecessary
-function getHumanChoice(humAns){
+// use function as TOP suggests
+function getHumanChoice(){
     if (humAns == "rock") {
         return "rock";
     }
@@ -33,7 +32,7 @@ function getHumanChoice(humAns){
     else {
         return "scissors";
     }
-}
+} 
 
 // display human answer
 console.log(getHumanChoice(humAns));
@@ -42,40 +41,38 @@ console.log(getHumanChoice(humAns));
 let computerScore = 0;
 let humanScore = 0;
 
+function playGame(humAns, compAns){
 // compare human answer to computer answer and display win or lose
-function playRound() {
-    if (getHumanChoice(humAns) == "rock" &&  getComputerChoice(compAns) == "scissors") {
-        console.log("You win!");
-        ++ humanScore;
-        console.log(humanScore);
-        console.log(computerScore);
-    } else if (getHumanChoice(humAns) == "paper" && getComputerChoice(compAns) == "rock") {
-        console.log("You win!!");
-        ++ humanScore;
-        console.log(humanScore);
-        console.log(computerScore);
-    } else if (getHumanChoice(humAns) == "scissors" && getComputerChoice(compAns) == "paper") {
-        console.log("You win!!!");
-        ++ humanScore;
-        console.log(humanScore);
-        console.log(computerScore);
-    } else {
-        console.log("You lose :(");
-        ++ computerScore;
-        console.log(humanScore);
-        console.log(computerScore);
+    function playRound() {
+        if (getHumanChoice == "rock" &&  getComputerChoice == "scissors") {
+            console.log("You win!");
+            ++ humanScore;
+            console.log(humanScore);
+            console.log(computerScore);
+        } else if (getHumanChoice == "paper" && getComputerChoice == "rock") {
+            console.log("You win!!");
+            ++ humanScore;
+            console.log(humanScore);
+            console.log(computerScore);
+        } else if (getHumanChoice == "scissors" && getComputerChoice == "paper") {
+            console.log("You win!!!");
+            ++ humanScore;
+            console.log(humanScore);
+            console.log(computerScore);
+        } else {
+            console.log("You lose :(");
+            ++ computerScore;
+            console.log(humanScore);
+            console.log(computerScore);
+        }
+        
     }
-    
-}
-
-function playGame() {
-    playRound();
-    playRound();
-    playRound();
     playRound();
     playRound();
 }
 
 playGame();
+
+
 
 
